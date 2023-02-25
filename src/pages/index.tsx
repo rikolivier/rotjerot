dbcfae9f856462ed0f1b42dd09d2fb9af9e76bf8
rolivier <rolivier@ispnext.com>
2023-02-25 08:55:14 +0100

    Add extra styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const handleLoading = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#fdfdfd] to-[#d3d4df]">
       <h2 className="text-3xl font-bold  text-gray-800">Rotje Rot</h2>
       <p>A explosive knowledge sharer and fermentation tracker in a visual way</p>
-      <button className="bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded" onClick={handleLoading}>{isLoading ? 'loading' : "Start fermentation"}</button>
+      <button className={[`bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded`, `${isLoading ? 'isLoading' : 'loaded'}`].join(' ')} onClick={handleLoading}>{"Start fermentation"}</button>
       </main>   
     </>
   );  
