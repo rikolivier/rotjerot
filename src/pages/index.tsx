fee3639f2a33b977e06f167ef91246843314bff5
rolivier <rolivier@ispnext.com>
2023-03-14 20:44:16 +0100

    Just some stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import { type NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
-// add isLoading state to button 
-// handleCreateFerment
-const [isLoading, setIsLoading] = useState(false);
-const handleLoading = () => {
-  setIsLoading(true);
-  setTimeout(setIsLoading,1000) 
-};
+  // add isLoading state to button
+  // handleCreateFerment
+  const [isLoading, setIsLoading] = useState(false);
+  const handleLoading = () => {
+    setIsLoading(true);
+    setTimeout(setIsLoading, 1000);
+  };
   return (
     <>
       <Head>
@@ -17,12 +17,22 @@ const handleLoading = () => {
         <meta name="description" content="Rotje Rot" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#fdfdfd] to-[#d3d4df]">
-      <h2 className="text-3xl font-bold  text-gray-800">Rotje Rot</h2>
-      <p>A explosive knowledge sharer and fermentation tracker in a visual way</p>
-      <button className={[`bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded`, `${isLoading ? 'isLoading' : 'loaded'}`].join(' ')} onClick={handleLoading}>{"Start fermentation"}</button>
-      </main>   
+        <h2 className="text-3xl font-bold  text-gray-800">Rotje Rot</h2>
+        <p>
+          A explosive knowledge sharer and fermentation tracker in a visual way
+        </p>
+        <button
+          className={[
+            `rounded bg-green-700 py-2 px-4 font-bold text-white hover:bg-green-600`,
+            `${isLoading ? "isLoading" : "loaded"}`,
+          ].join(" ")}
+          onClick={handleLoading}
+        >
+          {"Start fermentation"}
+        </button>
+      </main>
     </>
-  );  
+  );
 };
 
 export default Home;
---
+++ todo.md
@@ -0,0 +1,5 @@
+Rotje Rot 2023
+
+- moving organisms
+- recycle font's SteagelRough-Li and BrandonText-Medium
+- layers for blend mode difference (inspiration old RO logo)
