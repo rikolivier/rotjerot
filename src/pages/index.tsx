b10046957abcffa85547d2147e408c8088258dd3
rolivier <rolivier@ispnext.com>
2023-04-29 09:42:01 +0200

    Sketching adding images new design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import lilou from "./lilou.png"; // Tell webpack this JS file uses this image
 
 const Home: NextPage = () => {
   // add isLoading state to button
@@ -16,14 +18,15 @@ const Home: NextPage = () => {
         <title>Rotje Rot - 2023</title>
         <meta name="description" content="Rotje Rot" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#fdfdfd] to-[#d3d4df]">
-        <h2 className="text-3xl font-bold  text-gray-800">Rotje Rot</h2>
-        <p>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#000000] to-[#272727]">
+        <Image src={lilou} alt="LAB" width={100} />
+        <h2 className="text-3xl font-bold  text-white">I am alive</h2>
+        {/* <p>
           A explosive knowledge sharer and fermentation tracker in a visual way
-        </p>
+        </p> */}
         <button
           className={[
-            `rounded bg-green-700 py-2 px-4 font-bold text-white hover:bg-green-600`,
+            `rounded bg-purple-900 py-2 px-4 font-bold text-white hover:bg-purple-600`,
             `${isLoading ? "isLoading" : "loaded"}`,
           ].join(" ")}
           onClick={handleLoading}
--- todo.md
+++ todo.md
@@ -1,5 +1,9 @@
 Rotje Rot 2023
 
-- moving organisms
+- splash page I'm a live
+  - moving organisms/bactery
+  - on touch or click goto login page
+- login page or save to in browser data \*later
 - recycle font's SteagelRough-Li and BrandonText-Medium
 - layers for blend mode difference (inspiration old RO logo)
+-
